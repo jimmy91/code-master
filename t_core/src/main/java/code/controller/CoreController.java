478d5c47fb87dfb22cda9de40cd85235d17a35f2
jimmy91 <yangjinyu@palap.cn>
2023-02-17 14:48:50 +0800

    redis消费队列
    Signed-off-by: jimmy91 <yangjinyu@palap.cn>

--- t_core/src/main/java/code/controller/CoreController.java
+++ t_core/src/main/java/code/controller/CoreController.java
@@ -1,19 +1,16 @@
 package code.controller;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.lang.UUID;
 import code.queue.redis.RedisSender;
 import code.queue.redis.RedisUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import utils.generator.common.dao.vo.CommonResult;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Jimmy
@@ -31,9 +28,9 @@ public class CoreController {
     private RedisSender redisSender;
 
     @ApiOperation(value = "发送redis消息", notes="")
-    @PostMapping("/sendMsg/{channel}/{message}")
-    public CommonResult<Boolean> saveRedis(@PathVariable("topic") String channel, @PathVariable("message") String message) {
-        redisSender.sendChannelMess(channel, message);
+    @PostMapping("/sendMsg/{topic}/{message}")
+    public CommonResult<Boolean> saveRedis(@PathVariable("topic") String topic, @PathVariable("message") String message) {
+        redisSender.sendChannelMess(topic, message);
         return CommonResult.success(true);
     }
 
