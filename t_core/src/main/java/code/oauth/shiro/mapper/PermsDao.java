b170a164f42c0cfd012af83f6cc2051a30a910ae
Jimmy <yangjinyu@palap.cn>
2023-03-03 16:22:55 +0800

    23种设计模式
    Signed-off-by: Jimmy <yangjinyu@palap.cn>

--- t_core/src/main/java/code/oauth/shiro/mapper/PermsDao.java
+++
@@ -1,9 +0,0 @@
-package code.oauth.shiro.mapper;
-
-import code.oauth.shiro.pojo.Perms;
-import code.oauth.shiro.pojo.PermsExample;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PermsDao extends MyBatisBaseDao<Perms, Integer, PermsExample> {
-}
--- t_core/src/main/java/code/oauth/shiro/mapper/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package code.oauth.shiro.mapper;
-
-import code.oauth.shiro.pojo.Role;
-import code.oauth.shiro.pojo.RoleExample;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RoleDao  extends MyBatisBaseDao<Role, Integer, RoleExample> {
-}
